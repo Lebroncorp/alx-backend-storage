f7191b1db1f9e6dddfaba157024f132ddc14ca1d
Lebroncorp <lebroncorp@yahoo.com>
2024-04-30 02:56:50 +0000

    holberton school

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -12,7 +12,6 @@ def print_nginx_request_logs(nginx_collection):
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-
     for method in methods:
         req_count = len(list(nginx_collection.find({'method': method})))
         print('\tmethod {}: {}'.format(method, req_count))
@@ -28,6 +27,7 @@ def run():
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
     print_nginx_request_logs(client.logs.nginx)
+
 
 if __name__ == '__main__':
     run()
