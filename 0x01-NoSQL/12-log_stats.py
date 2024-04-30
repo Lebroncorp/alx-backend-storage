6f4769a1df8fe86a35cf2375878cb5358ef1d6d3
Lebroncorp <lebroncorp@yahoo.com>
2024-04-30 02:58:28 +0000

    holberton school

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-"""
-Task 12's module.
-"""
+'''Task 12's module.
+'''
 from pymongo import MongoClient
 
 
 def print_nginx_request_logs(nginx_collection):
-    """
-    Prints stats about Nginx request logs.
-    """
+    '''Prints stats about Nginx request logs.
+    '''
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
@@ -22,9 +20,8 @@ def print_nginx_request_logs(nginx_collection):
 
 
 def run():
-    """
-    Provides some stats about Nginx logs stored in MongoDB.
-    """
+    '''Provides some stats about Nginx logs stored in MongoDB.
+    '''
     client = MongoClient('mongodb://127.0.0.1:27017')
     print_nginx_request_logs(client.logs.nginx)
 
